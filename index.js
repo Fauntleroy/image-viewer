bc07bcf8cc37260ff57fd3fc28c4b78f3c85bf7c
Timothy Kempf <the.kaempfer@gmail.com>
2015-07-26 18:13:35 -0700

    beginnings of the image viewer itself

--- index.js
+++ index.js
@@ -14,13 +14,41 @@ var fetchJSON = function (url, callback) {
     request.send();
 };
 
+var ImageViewer = function (element, photos) {
+    this.el = element;
+    this.previous_el = element.querySelector('.image-viewer__previous');
+    this.next_el = element.querySelector('.image-viewer__next');
+    this.image_el = element.querySelector('.image-viewer__image');
+    this.name_el = element.querySelector('.image-viewer__name');
+
+    var initial_photo = photos[0];
+    this.displayImage(initial_photo.image_url, initial_photo.name);
+};
+
+ImageViewer.prototype = {
+    previousImage: function () {
+
+    },
+    nextImage: function () {
+
+    },
+    displayImage: function (url, name) {
+        this.image_el.src = url;
+        this.name_el.textContent = name;
+    }
+};
+
 var photosURL = 'https://api.500px.com/v1/photos'
     +'?feature=user'
     +'&username=fauntle'
     +'&sort=created_at'
-    +'&image_size=3'
+    +'&image_size=4'
     +'&consumer_key=caIubSYfdDLoEpy0Q0uxuIpeAoBXdseD6NmfHFYL';
 
-fetchJSON(photosURL, function(err, response, xhr){
-    console.log(err, response, xhr);
+document.addEventListener('DOMContentLoaded', function () {
+    var image_viewer_el = document.querySelector('#image-viewer');
+
+    fetchJSON(photosURL, function(err, response, xhr){
+        var image_viewer = new ImageViewer(image_viewer_el, response.photos);
+    });
 });
